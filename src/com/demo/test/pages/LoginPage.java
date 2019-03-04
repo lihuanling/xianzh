f03fd410abaf738a85da46ccfef3b1cdfcdd063c
Administrator <Administrator@PC-201807211302>
2019-03-04 14:04:07 +0800

    update

--- src/com/demo/test/pages/LoginPage.java
+++ src/com/demo/test/pages/LoginPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.By;
  * */
 public class LoginPage {
 	/**用户名输入框*/
-	public static final By ZH_INPUT_USERNAME = By.xpath(".//*[@id='components-layout-demo-top-side-2']/div/div/div[2]/div/div/form/div[1]/div/div/input");
+	public static final By ZH_INPUT_USERNAME = By.cssSelector(".el-card__body>div>form>div>div>div>input");//By.xpath(".//*[@id='components-layout-demo-top-side-2']/div/div/div[2]/div/div/form/div[1]/div/div/input");
 	
 	/**密码输入框*/
 	public static final By ZH_INPUT_PASSWORD = By.xpath(".//*[@id='components-layout-demo-top-side-2']/div/div/div[2]/div/div/form/div[2]/div/div/input");
